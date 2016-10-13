7142294ec235137827ff4f0dd509e87ac75f06bc
fangxu <gfx123321@126.com>
2016-10-13 13:16:37 +0800

    Initial Commit

---
+++ LzWeidget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LzWeidget
+//
+//  Created by ios开发 on 2016/10/13.
+//  Copyright © 2016年 oilchem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LzWeidget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LzWeidget
+//
+//  Created by ios开发 on 2016/10/13.
+//  Copyright © 2016年 oilchem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
